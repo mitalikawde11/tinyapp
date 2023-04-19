4fe32c0895e8169bbad627713481ffd330381b64
Mitali <mitali.kawde11@gmail.com>
2023-04-18 21:22:45 -0700

    Moving generateRandomString and urlsForUser functions from express_server.js into helpers.js

--- helpers.js
+++ helpers.js
@@ -1,3 +1,9 @@
+// generate a random short URL ID (random string)
+function generateRandomString() {
+  const randomStr = Math.random().toString(32).substring(2, 8);
+  return randomStr;
+};
+
 // return the user by looking up email address in database
 function getUserByEmail(email, usersDB) {
   for (const key in usersDB) {
@@ -8,5 +14,15 @@ function getUserByEmail(email, usersDB) {
   return;
 };
 
+// function returns the URLs where the userID is equal to the id of the currently logged-in user
+function urlsForUser(id, urlDB) {
+  let result = {};
+  for (const key in urlDB) {
+    if (urlDB[key].userID === id) {
+      result[key] = urlDB[key];
+    }
+  }
+  return result;
+};
 
-module.exports = { getUserByEmail };
+module.exports = { generateRandomString, getUserByEmail, urlsForUser };
